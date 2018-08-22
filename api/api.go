4f5028be865d94fc504e47b09a8d24db2ea882db
Hamilton Lima <hamilton.lima@indexexchange.com>
2018-08-22 15:47:40 -0400

    Adds GORM and updates GET and POST /books

--- api/api.go
+++ api/api.go
@@ -1,27 +1,33 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+)
 
 type Book struct {
+	gorm.Model
 	Name string
 }
 
 func main() {
-	books := []Book{
-		{Name: "Go Lang basics"},
-		{Name: "Gin HTTP"},
-	}
+
+	db, _ := gorm.Open("sqlite3", "books.db")
+	defer db.Close()
+	db.AutoMigrate(&Book{})
 
 	r := gin.Default()
 	r.GET("/books", func(c *gin.Context) {
+		var books []Book
+		db.Find(&books)
 		c.JSON(200, books)
 	})
 
 	r.POST("/books", func(c *gin.Context) {
 		var book Book
 		c.BindJSON(&book)
-		books = append(books, book)
-
+		db.Create(&book)
 		c.JSON(200, book)
 	})
 
