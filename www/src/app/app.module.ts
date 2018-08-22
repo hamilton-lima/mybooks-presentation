a7c83c09aaa35ed73efa2817cda287418bee9184
Hamilton Lima <hamilton.lima@indexexchange.com>
2018-08-22 16:06:11 -0400

    Creates empty components and services

--- www/src/app/app.module.ts
+++ www/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BooksComponent } from './books/books.component';
+import { AddBooksComponent } from './add-books/add-books.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BooksComponent,
+    AddBooksComponent
   ],
   imports: [
     BrowserModule
