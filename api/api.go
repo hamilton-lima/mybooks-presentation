4259053623d78cec4724ace05f35f3875a2b31be
Hamilton Lima <hamilton.lima@indexexchange.com>
2018-08-22 15:43:11 -0400

    Add GET /books

--- api/api.go
+++ api/api.go
@@ -2,12 +2,20 @@ package main
 
 import "github.com/gin-gonic/gin"
 
+type Book struct {
+	Name string
+}
+
 func main() {
+	books := []Book{
+		{Name: "Go Lang basics"},
+		{Name: "Gin HTTP"},
+	}
+
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
+	r.GET("/books", func(c *gin.Context) {
+		c.JSON(200, books)
 	})
+
 	r.Run()
 }
